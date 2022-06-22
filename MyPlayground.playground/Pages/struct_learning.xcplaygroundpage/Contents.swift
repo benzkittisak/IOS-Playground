216a7cbf9806dfb0a1a843f72b53243885df4cf9
benzkittisak <benzkittisak@stv.ac.th>
2022-06-22 16:10:58 +0700

    update code

--- MyPlayground.playground/Pages/struct_learning.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/struct_learning.xcplaygroundpage/Contents.swift
@@ -9,11 +9,19 @@ struct Town {
         self.resources = resources
     }
     
+//    return ค่าเป็น String หรือเป็น Type อะไรก็ใส่เป็น -> อะไรทำนองนั้นเลยนา
+    func joinTheArray()->String{
+//        ฟังก์ชัน join array เหมือนใน JS แต่ใน Swift จะใช้เป็น .joined(separator:....) อะไรแบบนี้
+        return citizens.joined(separator: ",")
+    }
+    
     func printDemo(){
-        print("\(name) มีประชาการคือ \(citizens) และมีทรัพยากรได้แก่ \(resources)")
+        print("\(name) มีประชาการคือ \(joinTheArray()) และมีทรัพยากรได้แก่ \(resources)")
     }
 }
 
 var myTown = Town(name: "Meow", citizens: ["Cat1" , "Cat2"], resources: ["Sand":300])
 myTown.citizens.append("Cat3")
 print(myTown.citizens)
+
+myTown.printDemo()
